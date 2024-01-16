b15d23e2b6ad85ed1749076346750489be34fd9d
Felipe Fernandes <felipekurosaki123@gmail.com>
2024-01-16 16:42:45 -0300

    create my portfolio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,57 @@
-import Image from 'next/image'
+"use client";
+
+import { Wrapper } from "@/components/wrapper";
+import profileImg from "../../public/picture-profile.jpg";
+import saveAs from "file-saver";
+import Image from "next/image";
 
 export default function Home() {
+  function handleDownloadPdf() {
+    const pdfURL = "/pdfs/cv-code.pdf";
+
+    fetch(pdfURL)
+      .then((response) => response.blob())
+      .then((blob) => {
+        saveAs(blob, "Felipe CV.pdf");
+      })
+      .catch((error) => console.error("Erro ao baixar o PDF:", error));
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <Wrapper>
+      <section className="w-full items-center justify-center flex flex-col gap-4">
+        <h1 className="text-4xl font-bold bg-gradient-to-r from-[#4f46e5] to-[#1d4ed8] bg-clip-text text-transparent mt-5">
+          Software Engineer
+        </h1>
+        <div className="w-full flex items-center justify-center flex-col">
+          <Image
+            src={profileImg}
+            alt={"my profile picture"}
+            quality={100}
+            objectFit="cover"
+            className="rounded-full h-56 w-56  "
+          />
+          <span className="w-full flex items-center text-center mt-4 font-bold text-lg max-w-5xl">
+            I am a software developer with a predominant focus on the front-end,
+            but I also have extensive knowledge of the back-end. I am looking
+            for an opportunity that allows me to face challenges and continue to
+            evolve with the company. I am ambitious and want to advance in my
+            professional career. I have a passion for programming and look
+            forward to contributing significantly to the team's success.
+          </span>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+        <div className="w-full flex flex-col items-center justify-center gap-4 md:flex-row mt-4">
+          <button
+            onClick={handleDownloadPdf}
+            className="hover:opacity-65 bg-gradient-to-r from-[#4f46e5] to-[#1d4ed8] p-3 text-lg font-bold rounded-full w-full md:max-w-52 text-center active:scale-95 transition-all duration-300"
+          >
+            Download cv
+          </button>
+          <button className=" hover:opacity-65 bg-transparent border-2 border-[#1d4ed8] text-[#1d4ed8] p-3 text-lg font-bold rounded-full w-full md:max-w-52 text-center active:scale-95 transition-all duration-300">
+            Contato
+          </button>
+        </div>
+      </section>
+    </Wrapper>
+  );
 }
---
+++ src/components/navbar/index.tsx
@@ -0,0 +1,69 @@
+"use client";
+
+import { cn } from "@/lib/cn";
+import { motion } from "framer-motion";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { useState } from "react";
+import { FaGripfire } from "react-icons/fa6";
+export function Navbar() {
+  const pages = ["about", "projects", "contacts"] as const;
+  const pathname = usePathname();
+  const [hovered, setHovered] = useState("");
+  return (
+    <header className=" z-30 mt-3 flex min-h-[3.75rem] w-full flex-wrap items-center text-xs text-white md:mt-0">
+      <Link
+        href="/"
+        className="order-1 ml-3 flex cursor-pointer appearance-none items-center justify-center rounded-lg border-none bg-transparent p-2 font-heading font-bold text-white transition-colors duration-200 ease-in-out hover:bg-hover"
+      >
+        <FaGripfire
+          size={44}
+          color="#fff"
+          className="hover:opacity-60 transition-all duration-300"
+        />
+      </Link>
+      <nav className="order-3 flex-1 basis-full overflow-x-auto overflow-y-hidden text-center md:order-2 md:basis-[initial]">
+        <ul className="relative top-[5px] inline-flex list-none justify-around">
+          {pages.map((page) => {
+            const path = `/${page.toLowerCase()}`;
+            const isHovered = hovered === page;
+            return (
+              <li key={page}>
+                <Link
+                  href={path}
+                  className="border-0 hover:opacity-100 focus:opacity-100"
+                >
+                  <motion.span
+                    className={cn(
+                      "relative inline-block cursor-pointer p-5 text-xs font-medium uppercase tracking-[0.075em] text-secondary transition-colors duration-200 ease-in-out hover:text-primary",
+                      pathname === path && "text-primary after:opacity-100",
+                      "after:absolute after:left-0 after:right-0 after:top-[1.125rem] after:mx-auto after:h-px after:w-5 after:bg-white after:opacity-0 after:transition-opacity after:duration-200 after:ease-in-out after:content-['']",
+                    )}
+                    onHoverStart={() => setHovered(page)}
+                    onHoverEnd={() => setHovered("")}
+                  >
+                    {isHovered && (
+                      <motion.span
+                        layoutId="nav"
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        exit={{ opacity: 0 }}
+                        className="absolute left-0 right-0 top-[7px] -z-10 rounded-lg bg-[#111827] p-5"
+                      />
+                    )}
+                    {page}
+                  </motion.span>
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
+      <aside className="order-2 ml-auto mr-3 flex items-center">
+        <div className="flex cursor-pointer items-center justify-center rounded-lg bg-transparent p-2 ease-in-out hover:bg-hover text-3xl font-bold hover:opacity-60 transition-all duration-300">
+          ⌘
+        </div>
+      </aside>
+    </header>
+  );
+}
