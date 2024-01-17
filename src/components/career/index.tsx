b3278079f5ba90b52d2b141b295dc5ca75ca1bf7
Felipe Fernandes <felipekurosaki123@gmail.com>
2024-01-17 17:12:08 -0300

    adjust about

--- src/components/career/index.tsx
+++ src/components/career/index.tsx
@@ -10,7 +10,7 @@ export function Career({
   icon,
 }: Job) {
   return (
-    <article className="w-full flex flex-col p-4 border-2 border-slate-600 md:max-w-[300px] rounded-lg shadow-md min-h-[160px]">
+    <article className="w-full flex flex-col p-4 border-2 border-slate-600 md:max-w-[300px] rounded-lg shadow-md min-h-[160px] hover:scale-105 transition-all duration-300 cursor-pointer">
       <div className="flex w-full gap-2 items-center h-full">
         <h1 className=" w-full text-lg font-bold bg-gradient-to-r from-[#db2777] to-[#e879f9] bg-clip-text text-transparent whitespace-pre-wrap">
           {jobTitle}
@@ -19,7 +19,7 @@ export function Career({
           {icon as unknown as ReactNode}
         </button>
       </div>
-      <section className="w-full flex md:flex-col mt-2">
+      <section className="w-full flex items-center justify-between md:items-start md:flex-col mt-2">
         <h1 className="text-lg font-bold bg-gradient-to-r from-[#0ea5e9] to-[#22d3ee] bg-clip-text text-transparent">
           {company}
         </h1>
