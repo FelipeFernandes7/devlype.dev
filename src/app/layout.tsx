d099378a6789c8eaaffc2309a8184be631be7896
Felipe Fernandes <felipekurosaki123@gmail.com>
2024-01-17 13:04:55 -0300

    adjust metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,26 +9,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "Felipe Fernandes",
   creator: "Felipe Fernandes",
-  alternates: {
-    canonical: "https://devlype.dev",
-    languages: {
-      en: "https://devlype.dev",
-    },
-  },
-
-  authors: [
-    {
-      name: "Felipe Fernandes",
-      url: "https://devlype.dev",
-    },
-  ],
   keywords: ["Felipe Fernandes", "devlype", "devlypedotdev", "devlype.dev"],
-  openGraph: {
-    type: "website",
-    title: "Felipe Fernandes",
-    locale: "en_US",
-    url: "https://devlype.dev",
-  },
 } satisfies Metadata;
 
 export default function RootLayout({
